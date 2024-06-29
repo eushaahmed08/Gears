dc61df0d74272f849485a47e1f2c5c840a25d255
eushaahmed08 <eushaahmed08@gmail.com>
2024-06-30 01:28:44 +0600

    v3 navbar done

--- gears/src/components/navbar/navbar.js
+++ gears/src/components/navbar/navbar.js
@@ -3,53 +3,39 @@ import logo from "../../assets/logo.png"
 import "./Navbar.css"
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <>
     <div className="navbarContainer">
-        <div className="navbar">
+      <div className="navbar">
 
-            {/* adding logo */}
-
-            <div className="logo">
-                <img src={logo} alt=""/>
-            </div>
-
-
-            {/* adding links */}
-
-            <div className='links'>
-                <a href="/">Home</a>
-                <a href="/">About</a>
-                <a href="/">Blog</a>
-                <a href="/">Shop</a>
-                <a href="/">Contact</a>
-            </div>
-
-
-            {/* add icons */}
-            <div className="icons">
-                <sup>2</sup>
-                <ShoppingBagOutlinedIcon sx={{fontsize : "28px"}}/>
-
-            </div>
-
-
-            {/* adding auth */}
-            <div className="authButtons">
-                <button>Sign Up</button>
-                <button>Log in</button>
-
-            </div>
-
-
-            
+        {/* adding logo */}
+        <div className="logo">
+          <img src={logo} alt="logo"/>
         </div>
-    </div>
 
-      
-    </>
+        {/* adding links */}
+        <div className='links'>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Blog</a>
+          <a href="/">Shop</a>
+          <a href="/">Contact</a>
+        </div>
+
+        {/* add icons */}
+        <div className="icons">
+          <sup>2</sup>
+          <ShoppingBagOutlinedIcon sx={{ fontSize: "28px" }}/>
+        </div>
+
+        {/* adding auth */}
+        <div className="authContainer">
+          <button className="authButton">Sign Up</button>
+          <button className="authButton">Log In</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default navbar
+export default Navbar
