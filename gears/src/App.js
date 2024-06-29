12ac04293f024b804fa169742e1efce60ddb22fb
eushaahmed08 <eushaahmed08@gmail.com>
2024-06-30 04:11:08 +0600

    searching demo

--- gears/src/App.js
+++ gears/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Navbar from './components/navbar/Navbar'; // Ensure the import statement matches the file name
-import {Routes , Route} from "react-router-dom"
+import Navbar from './components/navbar/Navbar';
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Blog from "./pages/blog/Blog";
@@ -11,23 +11,25 @@ import Cart from "./pages/cart/Cart";
 import SignUp from "./pages/signup/SignUp";
 import LogIn from "./pages/login/LogIn";
 
-
-
-
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar onSearch={handleSearch} />
       <Routes>
-        <Route path='/'element={<Home/>}></Route>
-        <Route path='/about'element={<About/>}></Route>
-        <Route path='/blog'element={<Blog/>}></Route>
-        <Route path='/shop'element={<Shop/>}></Route>
-        <Route path='/contact'element={<Contact/>}></Route>
-        <Route path='/cart'element={<Cart/>}></Route>
-        <Route path='/login'element={<LogIn/>}></Route>
-        <Route path='/signup'element={<SignUp/>}></Route>
-        
+        <Route path='/' element={<Home searchTerm={searchTerm} />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
     </div>
   );
--- gears/src/components/navbar/navbar.js
+++ gears/src/components/navbar/navbar.js
@@ -1,42 +1,66 @@
-import React from 'react'
-import logo from "../../assets/logo.png"
-import "./Navbar.css"
+import React from 'react';
+import logo from "../../assets/logo.png";
+import "./Navbar.css";
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
-import {Link} from "react-router-dom";
-const Navbar = () => {
+import SearchIcon from '@mui/icons-material/Search';
+import { Link } from "react-router-dom";
+
+const Navbar = ({ onSearch }) => {
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const searchTerm = e.target.elements.search.value;
+    onSearch(searchTerm);
+  };
+
   return (
     <div className="navbarContainer">
       <div className="navbar">
-
-        {/* adding logo */}
         <div className="logo">
-          <img src={logo} alt="logo"/>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
         </div>
 
-        {/* adding links */}
-        <div className='links'>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/shop">Shop</Link>
-          <Link to="/contact">Contact</Link>
-
+        <div className="links">
+          <Link to="/" className="navLink">Home</Link>
+          <Link to="/about" className="navLink">About</Link>
+          <Link to="/blog" className="navLink">Blog</Link>
+          <Link to="/shop" className="navLink">Shop</Link>
+          <Link to="/contact" className="navLink">Contact</Link>
         </div>
 
-        {/* add icons */}
+        <div className="search">
+          <form onSubmit={handleSearch} className="searchForm">
+            <input
+              type="text"
+              placeholder="Search products..."
+              name="search"
+              className="searchInput"
+            />
+            <button type="submit" className="searchButton">
+              <SearchIcon />
+            </button>
+          </form>
+        </div>
+
         <div className="icons">
           <sup>2</sup>
-          <Link to='/cart'><ShoppingBagOutlinedIcon sx={{ fontSize: "28px" }}/></Link>
+          <Link to="/cart" className="navLink">
+            <ShoppingBagOutlinedIcon sx={{ fontSize: "28px" }} />
+          </Link>
         </div>
 
-        {/* adding auth */}
         <div className="authContainer">
-          <Link to='/signup'><button className="authButton">Sign Up</button></Link>
-          <Link to='/login'><button className="authButton">Log In</button></Link>
+          <Link to="/signup">
+            <button className="authButton">Sign Up</button>
+          </Link>
+          <Link to="/login">
+            <button className="authButton">Log In</button>
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
