ea7af980abeaee45816ae6e28ef728b7f58c7eaa
eushaahmed08 <eushaahmed08@gmail.com>
2024-07-16 08:39:33 +0600

    resurge

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const colors = require('colors')
+
+
+//rest object
+const app = express();
+
+//rest api
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to ecommerce app</h1>");
+  });
+
+  //PORT
+const PORT = process.env.PORT || 8080;  
+
+
+//run listen
+app.listen(PORT, () => {
+    console.log(
+      `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+        .white
+    );
+  });
