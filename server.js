80fdf916f2879980bbe399e3acddf2db4c9c7d39
eushaahmed08 <eushaahmed08@gmail.com>
2024-08-25 02:02:55 +0600

    category

--- server.js
+++ server.js
@@ -4,28 +4,30 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import cors from "cors";
 
 //configure env
 dotenv.config();
 
-//database config
+//databse config
 connectDB();
 
 //rest object
 const app = express();
 
-//middlewares
+//middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
 //rest api
 app.get("/", (req, res) => {
-    res.send("<h1>Welcome to ecommerce app</h1>");
+  res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
 //PORT
@@ -33,7 +35,8 @@ const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, () => {
-    console.log(
-      `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
-    );
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
 });
