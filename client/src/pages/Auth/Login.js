11213bbd62ad4c3079a3204e7ab04ff086085c15
eushaahmed08 <eushaahmed08@gmail.com>
2024-09-06 09:36:03 +0600

    vercel

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", {
+      const res = await axios.post("https://gears-green.vercel.app/api/v1/auth/login", {
         email,
         password,
       });
