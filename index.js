61a3ae2ce8a03b50a5cd3dd1575a5315e94ac65c
eushaahmed08 <eushaahmed08@gmail.com>
2024-09-06 11:32:35 +0600

    adjusting api links

--- index.js
+++ index.js
@@ -23,10 +23,9 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use("https://gears-green.vercel.app/api/v1/auth", authRoutes);
-app.use("https://gears-green.vercel.app/api/v1/category", categoryRoutes);
-app.use("https://gears-green.vercel.app/api/v1/product", productRoutes);
-
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 //rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to GEARS</h1>");
